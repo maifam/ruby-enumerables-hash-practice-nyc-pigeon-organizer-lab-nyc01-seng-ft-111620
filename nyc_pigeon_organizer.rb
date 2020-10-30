8ce949a76ca01b03d775d8816dcbf3034c105307
Mai Pham <maitphm@gmail.com>
2020-10-30 21:22:23 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+
+ def nyc_pigeon_organizer(data)
+  
+  final = data.each_with_object({}) do |(key,value), new_list|
+    value.each do |attributes, names|
+      names.each do |name|
+        if !new_list[name]
+          new_list[name] = {} 
+        end 
+        if !new_list[name][key]
+           !new_list[name][key] = []
+        end 
+        new_list[name][key].push(attributes.to_s)
+      end 
+    end 
+  end
 end
+
+
+
+# new hash = names => color, gender, location  
